de125ef773385778cca64722927c634a62c18d60
Harrison Chen <harrison.chen@adlinktech.com>
2024-08-13 17:16:46 +0800

    To create a new tftp2sd library for accessing an SD card via TFTP

---
+++ tftp2sd.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "/tftp_client.h"
+#include "lwip/tftp_server.h"
+#include "src/SD-samd5x.h"
+
+void* tftp_open(const char* fname, const char* mode, u8_t is_write);
+void tftp_close(void* file_handler);
+int tftp_read(void* handle, void* buf, int bytes);
+int tftp_write(void* handle, struct pbuf* p);
+void tftp_error(void* handle, int err, const char* msg, int size);
+
+const struct tftp_context tftp = {
+  tftp_open,
+  tftp_close,
+  tftp_read,
+  tftp_write,
+  tftp_error
+};
+
+void ot2it_tftp_example_init_server(void);
